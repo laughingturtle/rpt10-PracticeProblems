29898945c571391f6e0fae3ce7dcda95ea522a7d
Magee Mooney <mageemooney@me.com>
2017-07-11 12:50:47 -0400

    (problem) pseudocoding

---
+++ problems/pseudocoding/pseudocoding.js
@@ -0,0 +1,68 @@
+// Write complete, unambiguous, and clear pseudocode instructions that 
+// a very obedient 4 year old could succeed at by following your 
+// instructions exactly.
+//
+// Once you complete the pseudocode for each prompt and finish the
+// implementation quickly. Time yourself. Strive to improve your time
+// with each subsequent problem. Practicing under time pressure is a
+// great way to prepare for job interviews down the line.
+//
+// For example:
+//
+// var times = function(n, iteratee, context) {};
+//
+// Invokes the given iteratee function n times. Each invocation of iteratee 
+// is called with an index argument. Produces an array of the returned 
+// values. 
+//
+// Pseudocode
+// 
+//   var times = function(n, iteratee, context) {
+//     create a results array
+//     create a loop that counts to n
+//       for each index of loop call the iteratee
+//       place return of each of the calls in the results array
+//     return results array
+//   };
+//
+// Implementation:
+//
+//   var times = function(n, iteratee, context) {
+//     var results = [];
+//     for ( var i =0; i < n; i++ ) {
+//       var sol = iteratee();
+//       results.push(sol);
+//     return results;
+//   }
+//
+// Let's go!  Here are your functions to pseudocode:
+
+// input:  string
+// output: boolean
+var isPalindrome = function () {
+
+};
+
+// input:  array of strings
+// output: string
+var longestWord = function () {
+
+};
+
+// input: string
+// output: letter
+var mostRepeatedCharacter = function () {
+
+};
+
+// input: number
+// output: boolean
+var isPrime = function () {
+
+};
+
+// input: number
+// output: number
+var modulusDivide = function () {
+
+};
