42b4157f1f65825bc6c3744d4ae4928023d0046e
Monkey Do <monkeydo@Monkeys-MacBook-Pro.local>
2018-11-29 20:27:37 -0800

    Grocery List - added strike-through capacity

--- exercises/GroceryList/client/src/components/GroceryItem.jsx
+++ exercises/GroceryList/client/src/components/GroceryItem.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 
-const GroceryItem = (props) => (
-  <div>
-    {/* {console.log('props in Groc Item', props.description)} */}
-    {props.description} - <strong>{props.quantity}</strong>
-  </div>
-)
+export default class GroceryList extends React.Component  {
 
-export default GroceryItem;
+  constructor(props)  {
+      super(props);
+
+      this.state = {
+          click: false
+        };
+  }
+
+  onListItemClick() {
+    this.setState({
+      click: !this.state.click
+    });
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.click ? 'line-through' : 'none'
+    }
+
+    return(
+        <div>
+          {/* {console.log('props in Groc Item', props.description)} */}
+          <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.description} - <strong>{this.props.quantity}</strong></li>
+        </div>
+      );
+  }
+}
